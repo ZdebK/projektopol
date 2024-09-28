d03d32f53cc00fd301a70bc13764d08890117086
katarzyna.zdeb@gmail.com <katarzyna.zdeb@b4bridge.com>
2024-09-28 17:02:30 +0000

    #next

--- src/pluginDialog.js
+++ src/pluginDialog.js
@@ -1,8 +1,6 @@
 async function redirectToUrl() {
     // Change the button class to "disabled"
     const button = document.getElementById("toggle-btn");
-    button?.classList.add("disabled"); // Add disabled class
-    button?.classList.remove("clickable"); // Remove clickable class
 
     try {
         // Send a POST request to the API Gateway URL
@@ -18,7 +16,7 @@ async function redirectToUrl() {
             })
             .catch(error => {
                 console.error('There has been a problem with your fetch operation:', error);
-                document.getElementById('output-info').innerText = 'Error fetching cat fact!';
+                document.getElementById('toggle-btn').innerText = 'Error fetching cat fact!';
             });
     } catch (error) {
         console.error('Error:', error);
