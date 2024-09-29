cb79199ee7f4eba51488cce4dc27e3d70cb84943
Krzysztof Gądek <krz.gdk@gmail.com>
2024-09-29 09:12:50 +0200

    fix lambda await

--- src/lambda.js
+++ src/lambda.js
@@ -1,9 +1,8 @@
 import { check } from "./check";
 
-
 exports.handler = async (event) => {
     
     // Extract the URL from the event input
     const url = event.url;
-    return check(url);
+    return await check(url);
 };
